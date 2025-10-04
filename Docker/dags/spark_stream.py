9d75af04c70f5db1017f2e441c24e6301e40d42d
copter50029 <152718101+copter50029@users.noreply.github.com>
2025-10-04 16:00:57 +0700

    Update spark_stream.py

--- Docker/dags/spark_stream.py
+++ Docker/dags/spark_stream.py
@@ -85,4 +85,4 @@ with DAG(
         op_kwargs={"data": transform_task.output},
     )
     
-    extract_task >> transform_task >> load_task # pylint: disable=pointless-statement
+extract_task >> transform_task >> load_task  # pylint: disable=pointless-statement
